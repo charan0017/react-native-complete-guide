c5f6f1eb2974539e21c9c68209e7d1a4e7569685
Sai Charan <charan0017@gmail.com>
2020-03-02 17:17:50 +0530

    Completed Section 2

---
+++ components/GoalInput.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
+
+const GoalInput = ({ visible, setVisible, addGoalHandler }) => {
+  const [courseGoal, setCourseGoal] = useState('');
+
+  const courseGoalHandler = enteredText => {
+    setCourseGoal(enteredText);
+  };
+
+  const onAddGoalHandler = () => {
+    addGoalHandler(courseGoal);
+    setCourseGoal('');
+  };
+
+  return (
+    <Modal visible={visible} animationType='slide'>
+      <View style={styles.column}>
+        <TextInput placeholder='Course Goal' style={styles.input} value={courseGoal} onChangeText={courseGoalHandler} />
+        <View style={styles.row}>
+          <View style={styles.button}>
+            <Button title='Cancel' color='red' onPress={setVisible} />
+          </View>
+          <View style={styles.button}>
+            <Button title='Add' onPress={onAddGoalHandler} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  column: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 10,
+    width: '80%',
+    marginBottom: 10
+  },
+  button: {
+    width: '40%'
+  }
+});
+
+export default GoalInput;
---
+++ components/GoalItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const GoalItem = ({ id, title, onDelete }) => {
+  return (
+    <TouchableOpacity activeOpacity={0.8} onPress={onDelete.bind(this, id)}>
+      <View style={styles.listItem}>
+        <Text>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginTop: 10,
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: '#ccc'
+  }
+});
+
+export default GoalItem;
