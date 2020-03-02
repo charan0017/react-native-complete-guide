33d64e3e51a811b3aba5e0bcb9fc10fb602c346e
Sai Charan <charan0017@gmail.com>
2020-03-02 17:37:45 +0530

    Updated master branch for base layout to clone for different sections

--- App.js
+++ App.js
@@ -1,42 +1,24 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Button, FlatList } from 'react-native';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-  const [goals, setGoals] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const addGoalHandler = goalTitle => {
-    if (!goalTitle || !goalTitle.length) {
-      return;
-    }
-    setGoals(currentGoals => [...currentGoals, { id: Date.now().toString(), value: goalTitle }]);
-    closeAddGoalModal();
-  };
-
-  const removeGoalHandler = goalId => {
-    setGoals(currentGoals => currentGoals.filter(({ id }) => id !== goalId));
-  };
-
-  const closeAddGoalModal = () => {
-    setIsModalOpen(false);
-  };
-
+const App = () => {
   return (
     <View style={styles.container}>
-      <Button title='Add Goal Item' onPress={() => setIsModalOpen(true)} />
-      <GoalInput visible={isModalOpen} setVisible={closeAddGoalModal} addGoalHandler={addGoalHandler} />
-      <FlatList
-        data={goals}
-        renderItem={({ item }) => <GoalItem id={item.id} title={item.value} onDelete={removeGoalHandler} />}
-      />
+      <Text style={styles.text}>Its the base layout to clone for different branches</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    padding: 10,
+    fontSize: 12
   }
 });
+
+export default App;
