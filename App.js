c77696ce0164fe681bc6d17cbfebbdbcfc59de36
Sai Charan <charan0017@gmail.com>
2020-03-03 09:00:47 +0530

    Updated tab indent

--- App.js
+++ App.js
@@ -4,39 +4,39 @@ import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [goals, setGoals] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [goals, setGoals] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const addGoalHandler = goalTitle => {
-    if (!goalTitle || !goalTitle.length) {
-      return;
-    }
-    setGoals(currentGoals => [...currentGoals, { id: Date.now().toString(), value: goalTitle }]);
-    closeAddGoalModal();
-  };
+    const addGoalHandler = goalTitle => {
+        if (!goalTitle || !goalTitle.length) {
+            return;
+        }
+        setGoals(currentGoals => [...currentGoals, { id: Date.now().toString(), value: goalTitle }]);
+        closeAddGoalModal();
+    };
 
-  const removeGoalHandler = goalId => {
-    setGoals(currentGoals => currentGoals.filter(({ id }) => id !== goalId));
-  };
+    const removeGoalHandler = goalId => {
+        setGoals(currentGoals => currentGoals.filter(({ id }) => id !== goalId));
+    };
 
-  const closeAddGoalModal = () => {
-    setIsModalOpen(false);
-  };
+    const closeAddGoalModal = () => {
+        setIsModalOpen(false);
+    };
 
-  return (
-    <View style={styles.container}>
-      <Button title='Add Goal Item' onPress={() => setIsModalOpen(true)} />
-      <GoalInput visible={isModalOpen} setVisible={closeAddGoalModal} addGoalHandler={addGoalHandler} />
-      <FlatList
-        data={goals}
-        renderItem={({ item }) => <GoalItem id={item.id} title={item.value} onDelete={removeGoalHandler} />}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Button title='Add Goal Item' onPress={() => setIsModalOpen(true)} />
+            <GoalInput visible={isModalOpen} setVisible={closeAddGoalModal} addGoalHandler={addGoalHandler} />
+            <FlatList
+                data={goals}
+                renderItem={({ item }) => <GoalItem id={item.id} title={item.value} onDelete={removeGoalHandler} />}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 50
-  }
+    container: {
+        padding: 50
+    }
 });
