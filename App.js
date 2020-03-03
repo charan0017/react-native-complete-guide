68a8dbb719aee19e8aa9bea457c998010ea95d20
Sai Charan <charan0017@gmail.com>
2020-03-03 09:01:10 +0530

    Updated tab indent

--- App.js
+++ App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Its the base layout to clone for different branches</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Its the base layout to clone for different branches</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    padding: 10,
-    fontSize: 12
-  }
+    container: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        padding: 10,
+        fontSize: 12
+    }
 });
 
 export default App;
